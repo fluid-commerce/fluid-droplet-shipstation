b716940a72373eb0b30b03bbd545bf78078b859a
saulo <saulolazarogarcia@gmail.com>
2025-08-13 17:24:44 -0300

    update db config terraform

--- terraform/environment/production/db.tf
+++ terraform/environment/production/db.tf
@@ -2,7 +2,7 @@
 module "postgres_db_instance" {
   source = "../../modules/cloud_sql_postgres"
 
-  instance_name    = "fluid-droplet-avalara"
+  instance_name    = "fluid-droplet-shipstation"
   database_version = "POSTGRES_17"
   region           = var.region
 
