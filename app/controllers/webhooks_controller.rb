3c4306c276a6e7401ef33ed4568d65949900902e
Gaston Massimino <gamassimino01@gmail.com>
2025-08-20 10:27:56 -0300

    update webhook controller based on droplet-template

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authenticate_webhook_token, unless: :droplet_installed_for_first_time?
@@ -22,36 +20,38 @@ class WebhooksController < ApplicationController
   private
 
   def droplet_installed_for_first_time?
-    params[:resource] == 'droplet' && params[:event] == 'installed'
+    params[:resource] == "droplet" && params[:event] == "installed"
   end
 
   def authenticate_webhook_token
     company = find_company
     if company.blank?
-      render json: { error: 'Company not found' }, status: :not_found
+      render json: { error: "Company not found" }, status: :not_found
     elsif !valid_auth_token?(company)
-      render json: { error: 'Unauthorized' }, status: :unauthorized
+      render json: { error: "Unauthorized" }, status: :unauthorized
     end
   end
 
   def valid_auth_token?(company)
     # Check header auth token first, then fall back to params
-    auth_header = request.headers['AUTH_TOKEN'] || request.headers['X-Auth-Token']
-    return true if auth_header.present? && auth_header == company.webhook_verification_token
+    auth_header = request.headers["AUTH_TOKEN"] || request.headers["X-Auth-Token"] || request.env["HTTP_AUTH_TOKEN"]
+    webhook_auth_token = Setting.fluid_webhook.auth_token
 
-    # Fall back to webhook verification token in params
-    # TODO: Remove this we are confirmed to be using the auth token in the header
-    company_params[:webhook_verification_token].present? &&
-      company_params[:webhook_verification_token] == company.webhook_verification_token
+    auth_header.present? && auth_header == webhook_auth_token
   end
 
   def find_company
-    Company.find_by(company_droplet_uuid: company_params[:company_droplet_uuid]) ||
+    Company.find_by(droplet_installation_uuid: company_params[:droplet_installation_uuid]) ||
       Company.find_by(fluid_company_id: company_params[:fluid_company_id])
   end
 
   def company_params
-    params.require(:company).permit(:authentication_token, :droplet_installation_uuid, :droplet_uuid,
-                                    :fluid_company_id, :fluid_shop, :name, :webhook_verification_token)
+    params.require(:company).permit(
+      :company_droplet_uuid,
+      :droplet_installation_uuid,
+      :fluid_company_id,
+      :webhook_verification_token,
+      :authentication_token
+    )
   end
 end
